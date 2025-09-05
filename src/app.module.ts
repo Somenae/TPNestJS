fcf123d38e33cc8cce128f1d27c52d9741204234
Somenae <kevindevaux@neuf.fr>
2025-09-05 15:21:40 +0200

    logger writing in file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,21 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { LikesModule } from './likes/likes.module';
 import { PostsModule } from './posts/posts.module';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
 
 @Module({
   imports: [UsersModule, AuthModule, LikesModule, PostsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes('*')
+  }
+}
--- src/middlewares/logger.middleware.ts
+++ src/middlewares/logger.middleware.ts
@@ -1,5 +1,6 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
+import { appendFile, appendFileSync, createWriteStream, existsSync, writeFileSync } from 'fs';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
@@ -9,6 +10,18 @@ export class LoggerMiddleware implements NestMiddleware {
     const url = req.originalUrl;
     
     console.log(`[${timestamp}] ${method} ${url}`);
+
+    let path: string = "log.txt";
+
+    if (!existsSync(path)) {
+      let stream = createWriteStream(path);
+      stream.on('open', () => {
+        stream.write(`[${timestamp}] ${method} ${url}`);
+        stream.close();
+      });
+    } else {
+      appendFileSync(path, `\n[${timestamp}] ${method} ${url}`);
+    }
     
     next();
   }
